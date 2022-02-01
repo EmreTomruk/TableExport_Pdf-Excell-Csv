a209e99e5c0803fd10b03761a032c46b96d4452e
Emre <emretmrk39@gmail.com>
2022-02-01 17:18:36 +0300

    first-commit

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using TableExport_Pdf_Mvc_Word.Models.Entity;
+
+namespace TableExport_Pdf_Mvc_Word.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        MyBlogDbEntities _db = new MyBlogDbEntities();
+        public ActionResult Index()
+        {
+            var degerler = _db.Categories.ToList();
+
+            return View(degerler);
+        }
+
+        public ActionResult GetFilterByName()
+        {
+            var degerler = _db.Set<Categories>().Where(c => c.Name == "Vue.Js").ToList();
+
+            return View(degerler);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TableExport_Pdf_Mvc_Word.MvcApplication" Language="C#" %>
